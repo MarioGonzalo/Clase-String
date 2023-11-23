ade858982d0e63094100a08f4c05e21c53af00ad
MarioGonzalo <146821965+MarioGonzalo@users.noreply.github.com>
2023-11-23 13:00:31 +0100

    Añadir comentarios ejs 1, 2

--- Ejercicio1/main.cpp
+++ Ejercicio1/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    string sentence = "Hola mundo!";
-    cout << sentence << std::endl;
+    string sentence = "Hola mundo!";// Se crea una variable de tipo string y se le asigna un valor
+    cout << sentence << std::endl; // Se imprime la variable sentence
     return 0;
 }
--- Ejercicio2/main.cpp
+++ Ejercicio2/main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main() {
     string sentence;
-    cout << "Introduce una frase: ";
+    cout << "Introduce una frase: "; // Introducimos una frase
     getline(cin, sentence);
-    cout << "La frase tiene " << sentence.size()<< " caracteres" << endl;
-    cout << "¿Está la cadena vacía? " << system("pause")<< (sentence.empty() ? "Sí" : "No") << endl;
+    cout << "La frase tiene " << sentence.size()<< " caracteres" << endl;// Mostramos el tamaño de la frase
+    cout << "¿Está la cadena vacía? " << system("pause")<< (sentence.empty() ? "Sí" : "No") << endl; // Comprobamos si la cadena está vacía
     return 0;
 }
